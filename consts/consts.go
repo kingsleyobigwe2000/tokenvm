15d5954c168fd0f7233292a09be77b15b5b77b5c
kingsleyobigwe2000 <kingsleyobigwe2000@gmail.com>
2024-07-13 18:19:12 +0000

    kingcoin

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "KingObi"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "king"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "kingcoin"
 )
 
 var ID ids.ID
